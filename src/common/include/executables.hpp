ccfc5ac1662b6c5c04cda991586496949beeabbd
Erik Caldwell <erikcaell@gmail.com>
2026-03-05 23:03:44 -0500

    Universal launcher

---
+++ src/common/include/executables.hpp
@@ -0,0 +1,13 @@
+#ifndef EXECUTABLES_HPP
+#define EXECUTABLES_HPP
+
+#include <vector>
+#include <string>
+
+const std::vector<std::string> MANAGED_APPS = {
+    "example_file_lock.aarch64le.bin"
+};
+
+const std::string DRIVE_APP = "drive.aarch64le.bin";
+
+#endif // EXECUTABLES_HPP
